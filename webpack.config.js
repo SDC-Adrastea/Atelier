78b78fe744807064021bfb894e88d6e2521479e1
Aaron Miller <AaronMiller1855@gmail.com>
2022-11-17 14:00:56 -0700

    init - Create functional framework - Babel , Webpack, React , etc

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+module.exports = {
+   entry: '/client/src/index.jsx',
+   output: {
+      path: path.join(__dirname, '/client/dist'),
+      filename: 'bundle.js'
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+         },
+         {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }
+      ]
+   },
+   mode: 'development'
+}
