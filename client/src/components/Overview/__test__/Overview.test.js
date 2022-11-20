121dac6f4e2d2501e32d706b28f8981062bbad6c
mindiweik <mindiweik@gmail.com>
2022-11-20 12:46:55 -0700

    basic test file added and confirmed working

---
+++ client/src/components/Overview/__test__/Overview.test.js
@@ -0,0 +1,11 @@
+import Enzyme, { shallow, configure } from 'enzyme';
+import { Overview } from '../../Overview.jsx';
+import React, { useEffect, useState } from "react";
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+it("SHALLOW: renders Overview Widget without crashing", ()=> {
+  shallow(<Overview />);
+});
+
