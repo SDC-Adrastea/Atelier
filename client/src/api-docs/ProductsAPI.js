4aeb3190aa6248c839bf7ab7ad453c008e77b48f
Thomas Van <thomascvan11@gmail.com>
2022-11-30 18:28:26 -0800

    completed all GET requests for Reviews API

--- client/src/api-docs/ProductsAPI.js
+++ client/src/api-docs/ProductsAPI.js
@@ -1,6 +1,6 @@
 // Quick Link to this API Doc: https://learn-2.galvanize.com/cohorts/3414/blocks/94/content_files/Front%20End%20Capstone/project-atelier/products.md
 let axios = require('axios');
-const TOKEN = "replace this token";
+const TOKEN = process.env.API_TOKEN;
 
 let options = {
   method: 'get',
