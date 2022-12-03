a31ee6f8d2b634bb85a9ba6840d6cdeb2615292c
mindiweik <mindiweik@gmail.com>
2022-12-03 12:37:32 -0700

    updates to the main product-basics files

--- client/src/components/Overview/ProductCategoryTitle.jsx
+++ client/src/components/Overview/ProductCategoryTitle.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const ProductCategoryTitle = (props) => {
-  let title = props.product.name
-  let category = props.product.category
+  let title = props.name
+  let category = props.category
 
   return (
     <div className="category-title">
       <h4>Overview - Product Category / Title</h4>
-      <div className="title">{title}</div>
       <div className="category">{category}</div>
+      <div className="title"><h2>{title}</h2></div>
     </div>
   )
 }
