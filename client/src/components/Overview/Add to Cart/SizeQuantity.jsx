eaf6632c48222c556c652c8aa4d8f49357e3c68e
mindiweik <mindiweik@gmail.com>
2022-12-06 20:58:48 -0700

    minor changes to SizeQuantity

--- client/src/components/Overview/Add to Cart/SizeQuantity.jsx
+++ client/src/components/Overview/Add to Cart/SizeQuantity.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react'
 
 const SizeQuantity = (props) => {
   let styleOptions = props.style.skus
+  let skus = []
   console.log('props in SizeQuantity', props.style.skus)
 
   const [options, setOptions] = useState({})
 
   useEffect(() => {
     setOptions(styleOptions)
+    skus = Object.keys(skus)
   })
 
   return (
