f773bbf538fb77bc32f0b2e48d0780764f7def2e
Thomas Van <thomascvan11@gmail.com>
2022-12-08 20:24:00 -0800

    added visual representation for rating score distribution

--- client/src/components/Reviews/RatingsBreakdown.jsx
+++ client/src/components/Reviews/RatingsBreakdown.jsx
@@ -2,17 +2,18 @@ import React, {useState, useEffect} from 'react';
 
 
 export const RatingsBreakdown = (props) => {
-  // var reviews = props.reviews;
-  // console.log(reviews);
 
   const [ratingsObject, setRatings] = useState({
     5: 0,
     4: 0,
     3: 0,
     2: 0,
-    1: 0,
-    recommend: 0
+    1: 0
   });
+
+  const [recommendCount, setRecommend] = useState(0);
+
+  const [maxRatingCount, setMaxRatingCount] = useState(0);
 
   useEffect(() => {
     var tempObj = {
@@ -20,36 +21,35 @@ export const RatingsBreakdown = (props) => {
       4: 0,
       3: 0,
       2: 0,
-      1: 0,
-      recommend: 0
+      1: 0
     };
+    var recommend = 0;
+    var ratingCount = 0;
+
     props.reviews.forEach(review => {
       tempObj[review.rating]++;
       if (review.recommend) {
-        tempObj.recommend++
+        recommend++;
       }
     })
     setRatings(tempObj);
+    setRecommend(recommend);
+    setMaxRatingCount(Math.max(...Object.values(tempObj)));
   },[props.reviews])
 
   return (
     <div>
-      <h4>{parseInt(Math.round((ratingsObject.recommend/props.reviews.length)*100))}% of reviews recommend this product</h4>
-      <h4>
-      5 Stars: {ratingsObject[5]}
-      </h4>
-      <h4>
-      4 Stars: {ratingsObject[4]}
-      </h4>
-      <h4>
-      3 Stars: {ratingsObject[3]}
-      </h4>
-      <h4>
-      2 Stars: {ratingsObject[2]}
-      </h4>
-      <h4>
-      1 Star: {ratingsObject[1]}
-      </h4>
+      <h4>{parseInt(Math.round((recommendCount/props.reviews.length)*100))}% of reviews recommend this product</h4>
+      <h4>5 Stars:</h4>
+      <progress id="file" value={ratingsObject[5]/maxRatingCount*100} max="100"> </progress>
+      <h4>4 Stars:</h4>
+      <progress id="file" value={ratingsObject[4]/maxRatingCount*100} max="100"> </progress>
+      <h4>3 Stars:</h4>
+      <progress id="file" value={ratingsObject[3]/maxRatingCount*100} max="100"> </progress>
+      <h4>2 Stars:</h4>
+      <progress id="file" value={ratingsObject[2]/maxRatingCount*100} max="100"> </progress>
+      <h4>1 Star:</h4>
+      <progress id="file" value={ratingsObject[1]/maxRatingCount*100} max="100"> </progress>
     </div>
   )
 };
