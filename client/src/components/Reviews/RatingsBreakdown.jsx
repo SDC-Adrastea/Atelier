64d595d18a2c395781e7f8618d3e269728820500
Thomas Van <thomascvan11@gmail.com>
2022-12-09 13:39:23 -0800

    Removed console log from ReviewsAPI Fixed RatingsBreakdown bug

--- client/src/components/Reviews/RatingsBreakdown.jsx
+++ client/src/components/Reviews/RatingsBreakdown.jsx
@@ -10,10 +10,8 @@ export const RatingsBreakdown = (props) => {
     2: 0,
     1: 0
   });
-
   const [recommendCount, setRecommend] = useState(0);
-
-  const [maxRatingCount, setMaxRatingCount] = useState(0);
+  const [maxRatingCount, setMaxRatingCount] = useState(1);
 
   useEffect(() => {
     var tempObj = {
@@ -24,41 +22,42 @@ export const RatingsBreakdown = (props) => {
       1: 0
     };
     var recommend = 0;
-    var ratingCount = 0;
+    var maxRatingCount = 1;
 
     props.reviews.forEach(review => {
       tempObj[review.rating]++;
       if (review.recommend) {
         recommend++;
       }
+      maxRatingCount = Math.max(...Object.values(tempObj));
     })
     setRatings(tempObj);
     setRecommend(recommend);
-    setMaxRatingCount(Math.max(...Object.values(tempObj)));
+    setMaxRatingCount(maxRatingCount);
   },[props.reviews])
 
   return (
     <div>
       <h4>{parseInt(Math.round((recommendCount/props.reviews.length)*100))}% of reviews recommend this product</h4>
       <div id="parent">
-        <p class="child">5 Stars</p>
-        <progress class="child" id="file" value={ratingsObject[5]/maxRatingCount*100} max="100"> </progress>
+        <p className="child">5 Stars</p>
+        <progress className="child" id="file" value={ratingsObject[5]/maxRatingCount*100} max="100"> </progress>
       </div>
       <div id="parent">
-        <p class="child">4 Stars</p>
-        <progress class="child" id="file" value={ratingsObject[4]/maxRatingCount*100} max="100"> </progress>
+        <p className="child">4 Stars</p>
+        <progress className="child" id="file" value={ratingsObject[4]/maxRatingCount*100} max="100"> </progress>
       </div>
       <div id="parent">
-        <p class="child">3 Stars</p>
-        <progress class="child" id="file" value={ratingsObject[3]/maxRatingCount*100} max="100"> </progress>
+        <p className="child">3 Stars</p>
+        <progress className="child" id="file" value={ratingsObject[3]/maxRatingCount*100} max="100"> </progress>
       </div>
       <div id="parent">
-        <p class="child">2 Stars</p>
-        <progress class="child" id="file" value={ratingsObject[2]/maxRatingCount*100} max="100"> </progress>
+        <p className="child">2 Stars</p>
+        <progress className="child" id="file" value={ratingsObject[2]/maxRatingCount*100} max="100"> </progress>
       </div>
       <div id="parent">
-        <p class="child">1 Stars</p>
-        <progress class="child" id="file" value={ratingsObject[1]/maxRatingCount*100} max="100"> </progress>
+        <p className="child">1 Stars</p>
+        <progress className="child" id="file" value={ratingsObject[1]/maxRatingCount*100} max="100"> </progress>
       </div>
     </div>
   )
