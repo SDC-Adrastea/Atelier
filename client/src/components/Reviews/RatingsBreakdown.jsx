51d88e89155448ea30bcbce7b4f2c3d3ae64b966
Thomas Van <thomascvan11@gmail.com>
2022-12-08 16:22:45 -0800

    reorganized server routings and import placement

--- client/src/components/Reviews/RatingsBreakdown.jsx
+++ client/src/components/Reviews/RatingsBreakdown.jsx
@@ -2,26 +2,20 @@ import React, {useState, useEffect} from 'react';
 
 
 export const RatingsBreakdown = (props) => {
-  var reviews = props.reviews;
-  console.log(reviews);
+  // var reviews = props.reviews;
+  // console.log(reviews);
 
-  const [ratingsBreakdown, setRatings] = useState({
-    5: 0,
-    4: 0,
-    3: 0,
-    2: 0,
-    1: 0
-  });
-
-  useEffect(() => {
-    reviews.forEach((review, index) => {
-      console.log(review);
-    })
-  })
+  // const [ratingsObject, setRatings] = useState({
+  //   5: 0,
+  //   4: 0,
+  //   3: 0,
+  //   2: 0,
+  //   1: 0
+  // });
 
   // useEffect(() => {
-  //   console.log(ratingsBreakdown)
-  // })
+  //   setRatings({1:1})
+  // }[reviews])
 
   return (
     <div>
