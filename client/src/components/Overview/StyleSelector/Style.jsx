1ea39baa1ca2ccbba14d3fe208aa75ceaa02c64f
mindiweik <mindiweik@gmail.com>
2022-12-20 22:02:05 -0700

    added checkmark overlay to selected style image

--- client/src/components/Overview/StyleSelector/Style.jsx
+++ client/src/components/Overview/StyleSelector/Style.jsx
@@ -22,14 +22,31 @@ const styleImage = {
   borderRadius: '50%'
 }
 
+const checkOverlay = {
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
+  zIndex: '1'
+}
+
+const checkmark = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)'
+}
+
 const Style = (props) => {
   let style = props.style
   let toggled = props.toggled
   let altName = props.style.name
-  let styleName
+  let styleName, checked
 
   if (toggled.style_id === style.style_id) {
     styleName = style.name
+    checked = <div style={checkOverlay}><div style={checkmark}><img src="checkmark.png" alt="checkmark" width="20" /></div></div>
   }
 
   return (
@@ -38,6 +55,7 @@ const Style = (props) => {
         <div style={styleNameText}>{styleName}</div>
         <img src={style.photos[0].thumbnail_url} alt={altName} width="60" height="60" style={styleImage}
           onClick={() => props.onClick(style)} />
+          {checked}
       </div>
     </div>
   )
