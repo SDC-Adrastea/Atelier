2afb3e6f60f1ab9dc7c71983944da37921238345
Aaron Miller <AaronMiller1855@gmail.com>
2022-12-29 20:07:01 -0700

    add logic for conditional rendering on outfit carousel.

--- client/src/components/Related/OutfitCarousel.jsx
+++ client/src/components/Related/OutfitCarousel.jsx
@@ -13,20 +13,33 @@ const OutfitCarousel = (props) => {
 
   //state for viewing window
   const [leftOffset, setLeftOffset] = useState(0)
+  const [cardsClickedThrough, setCardClick] = useState(0)
+  const totalOffSet = props.yourOutfit.length * 265
+  const numberOfHiddenCardsStarting = (totalOffSet - 795) / 265 // number of cards not shown
 
   return (
     <div className="your-outfit-carousel-transparent-container">
       <div data-testid="outfit-carousel">
         <div className="overflow-window-icon-outfit">
           {leftOffset < 0 &&
-            <img src="leftArrow.png" className="related-previous-button" onClick={() => { setLeftOffset(leftOffset + 265) }} />
+            <img src="leftArrow.png" className="related-previous-button" onClick={() => {
+              setLeftOffset(leftOffset + 265)
+              setCardClick(cardsClickedThrough - 1)
+            }} />
           }
           {leftOffset >= 0 &&
             <img src="blank.png" className="related-previous-button" />
           }
           <img className="outfit-product-card-1" src="lucyButton.png" onClick={() => addToOutfitList()} />
           <div className="overflow-window-outfit">
-            <img src="rightArrow.PNG" className="yourOutfit-next-button" onClick={() => { setLeftOffset(leftOffset - 265) }} />
+            {
+              cardsClickedThrough < numberOfHiddenCardsStarting &&
+            <img src="rightArrow.PNG" className="yourOutfit-next-button" onClick={() => {
+              setLeftOffset(leftOffset - 265)
+              setCardClick(cardsClickedThrough + 1)
+            }
+            } />
+            }
             <div className="youroutfit-carousel-container" style={{ marginLeft: `${leftOffset}px` }}>
               {props.yourOutfit.map((item, index) => {
                 return (
