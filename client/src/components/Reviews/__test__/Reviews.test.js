9337729347a20454b05a3b5d678896cf4a6ace6b
Thomas Van <thomascvan11@gmail.com>
2022-12-31 11:11:25 -0800

    begin refactoring unit tests

--- client/src/components/Reviews/__test__/Reviews.test.js
+++ client/src/components/Reviews/__test__/Reviews.test.js
@@ -10,41 +10,43 @@ import React, { useEffect, useState } from "react";
 
 // subcomponents
 import { Reviews } from '../../Reviews.jsx'
+import { RatingsColumn } from '../../Reviews/RatingsColumn.jsx'
 
 // dummy data
 import { dummyReviews, dummyMetaReviews } from "../../../../../dummyTestData/reviewsDummy.js";
+import { dummyProductData } from "../../../../../dummyTestData/productDummy.js";
 
 describe('Unit Test: Reviews', () => {
 
   test('Confirm initial load of Reviews component', () => {
-    render(<Reviews productNum={71700} metadata={dummyMetaReviews} />)
+    render(<Reviews productNum={11} product={dummyProductData} metadata={dummyMetaReviews} />)
     const ReviewsElement = screen.getByTestId('reviews')
     expect(ReviewsElement).toBeInTheDocument()
   })
 
   test('Confirm initial load of RatingsColumn component', () => {
-    render(<Reviews productNum={71700} metadata={dummyMetaReviews} />)
+    render(<RatingsColumn productNum={11} averageRating={4.2} reviews={[dummyReviews]} metadata={dummyMetaReviews}/>)
     const RatingsColumnElement = screen.getByTestId('ratings-column')
     expect(RatingsColumnElement).toBeInTheDocument()
   })
 
-  test('Confirm initial load of ReviewsList component', () => {
-    render(<Reviews productNum={71700} metadata={dummyMetaReviews} />)
-    const ReviewsListElement = screen.getByTestId('reviews-list')
-    expect(ReviewsListElement).toBeInTheDocument()
-  })
+  // test('Confirm initial load of ReviewsList component', () => {
+  //   render(<Reviews productNum={71700} metadata={dummyMetaReviews} />)
+  //   const ReviewsListElement = screen.getByTestId('reviews-list')
+  //   expect(ReviewsListElement).toBeInTheDocument()
+  // })
 
-  test('Confirm initial load of RatingsBreakdown component', () => {
-    render(<Reviews productNum={71700} metadata={dummyMetaReviews} />)
-    const RatingsBreakdownElement = screen.getByTestId('ratings-breakdown')
-    expect(RatingsBreakdownElement).toBeInTheDocument()
-  })
+  // test('Confirm initial load of RatingsBreakdown component', () => {
+  //   render(<Reviews productNum={71700} metadata={dummyMetaReviews} />)
+  //   const RatingsBreakdownElement = screen.getByTestId('ratings-breakdown')
+  //   expect(RatingsBreakdownElement).toBeInTheDocument()
+  // })
 
-  test('Confirm initial load of ProductBreakdown component', () => {
-    render(<Reviews productNum={71700} metadata={dummyMetaReviews} />)
-    const ProductBreakdownElement = screen.getByTestId('product-breakdown')
-    expect(ProductBreakdownElement).toBeInTheDocument()
-  })
+  // test('Confirm initial load of ProductBreakdown component', () => {
+  //   render(<Reviews productNum={71700} metadata={dummyMetaReviews} />)
+  //   const ProductBreakdownElement = screen.getByTestId('product-breakdown')
+  //   expect(ProductBreakdownElement).toBeInTheDocument()
+  // })
 
 
 
