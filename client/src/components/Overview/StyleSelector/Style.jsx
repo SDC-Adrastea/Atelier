8a933cacec796782ffc53a6b5ee1b6643bd05a95
mindiweik <mindiweik@gmail.com>
2023-01-04 19:53:50 -0700

    css updates and notes added to style and style select

--- client/src/components/Overview/StyleSelector/Style.jsx
+++ client/src/components/Overview/StyleSelector/Style.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import * as css from './StyleSelectorCSS.jsx'
 
+import '../../../styles/overview.css'
 
 const Style = (props) => {
   let style = props.style
@@ -8,15 +8,16 @@ const Style = (props) => {
   let altName = props.style.name
   let styleName, checked
 
+  // set the checkmark for the selected style
   if (toggled.style_id === style.style_id) {
     styleName = style.name
-    checked = <div style={css.checkOverlay}><div style={css.checkmark}><img src="checkmark.png" alt="checkmark" width="20" height="100%" /></div></div>
+    checked = <div className="check-overlay"><div className="checkmark"><img src="checkmark.png" alt="checkmark" width="20" height="100%" /></div></div>
   }
 
   return (
-    <div style={css.thumbnail}>
-      <div style={css.thumbnailContainer}>
-        <img src={style.photos[0].thumbnail_url} alt={altName} width="60" height="60" style={css.styleImage}
+    <div className="style-thumbnail">
+      <div className="style-thumbnail-container">
+        <img src={style.photos[0].thumbnail_url} alt={altName} width="60" height="60" className="style-image"
           data-testid="style-image" onClick={() => props.onClick(style)} id="style-image-change" widgetname="Overview" />
           {checked}
       </div>
