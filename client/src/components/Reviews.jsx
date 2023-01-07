135691b3f49fe1a8c7e5b9c1011bea1be744fea1
Thomas Van <thomascvan11@gmail.com>
2023-01-06 17:21:16 -0800

    expansion of review rating filtering

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -10,12 +10,12 @@ export const Reviews = (props) => {
   const [reviewsSortBy, setSort] = useState('relevant');
   const [averageRating, setAverageRating] = useState(0);
 
-  // RATINGS FILTERING
+  // --------- RATINGS FILTERING --------- //
 
   // filter status
   const [ratingFilterStatus, setRatingFilterStatus] = useState(false);
   // filtered list
-  const [filterList, setFilterList] = useState([]);
+  const [filteredReviewList, setFilteredReviewList] = useState([]);
   // fiveStars
   const [fiveStarFilter, setFiveStarFilter] = useState(false);
   // fourStars
@@ -27,7 +27,41 @@ export const Reviews = (props) => {
   // oneStars
   const [oneStarFilter, setOneStarFilter] = useState(false);
 
-  const ratingFilterFunc = (star) => {
+  const ratingFilterFunc = (clicked) => {
+    // copy of existing filtered list that will be modified and used to reset state
+    let tempFilterList = [...filteredReviewList]
+
+    if (clicked === 5) {
+      // if filter for this rating is on
+      if (fiveStarFilter) {
+        // remove all reviews with rating of 5 from the list
+        tempFilterList.forEach((review, index) => {
+          if (review.rating === 5) {
+            tempFilterList.splice(index, 1);
+          }
+        })
+      // if filter for this rating is off
+      } else {
+        // add all reviews with rating of 5 to the filtered list
+        reviews.forEach((review) => {
+          if (review.rating === 5) {
+            tempFilterList.push(review);
+          }
+        })
+      }
+      // toggle rating filter status
+      setFiveStarFilter(!fiveStarFilter)
+    } else if (clicked === 4) {
+      setFourStarFilter(!fourStarFilter)
+    } else if (clicked === 3) {
+      setThreeStarFilter(!threeStarFilter)
+    } else if (clicked === 2) {
+      setTwoStarFilter(!twoStarFilter)
+    } else if (clicked === 1) {
+      setOneStarFilter(!oneStarFilter)
+    }
+    // replace filtered list with new filtered list
+    setFilteredReviewList(tempFilterList);
   }
 
   const clearFilters = () => {
@@ -37,9 +71,18 @@ export const Reviews = (props) => {
     setTwoStarFilter(false);
     setOneStarFilter(false);
     setRatingFilterStatus(false);
-
+    setFilteredReviewList([]);
   }
 
+  useEffect(() => {
+    if (oneStarFilter || twoStarFilter || threeStarFilter || fourStarFilter || fiveStarFilter) {
+      setRatingFilterStatus(true);
+    } else {
+      setRatingFilterStatus(false);
+    }
+  }, [filteredReviewList]);
+
+  // ------------------------------------------ //
 
   var metadata = props.metadata;
 
@@ -70,7 +113,7 @@ export const Reviews = (props) => {
         <RatingsColumn productNum={props.productNum} averageRating={averageRating} reviews={reviews} metadata={props.metadata}/>
       </div>
       <div>
-        <ReviewsList productNum={props.productNum} product={props.product} reviews={reviews} setReviews={setReviews} reviewsShowing={reviewsShowing} setReviewsShowing={setReviewsShowing} reviewsSortBy={reviewsSortBy} setSort={setSort} metadata={props.metadata}/>
+        <ReviewsList productNum={props.productNum} product={props.product} reviews={ratingFilterStatus ? filteredReviewList : reviews} setReviews={setReviews} reviewsShowing={reviewsShowing} setReviewsShowing={setReviewsShowing} reviewsSortBy={reviewsSortBy} setSort={setSort} metadata={props.metadata}/>
       </div>
     </div>
   </div>
